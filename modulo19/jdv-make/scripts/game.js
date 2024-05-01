d4416269b1dab87c814ef02d76464d2194145473
Pedro Willen <pedrownsmcpe@gmail.com>
2024-04-30 21:19:57 -0400

    Adding winner verification and alert

--- modulo19/jdv-make/scripts/game.js
+++ modulo19/jdv-make/scripts/game.js
@@ -1,11 +1,50 @@
 let board = ['', '', '', '', '', '', '', '', ''];
 let playerTime = 0;
 let symbols = ['o', 'x'];
+let gameOver = false;
 
 function handleMove (position) {
+    if (gameOver == true) {
+        return
+    }
+
     if (board[position] == '') {
         board[position] = symbols[playerTime];
 
-        playerTime == 0 ? playerTime = 1 : playerTime = 0;
+        gameOver = isWin();
+        if(!gameOver) {
+            playerTime == 0 ? playerTime = 1 : playerTime = 0;
+        } else {
+            playerTime == 0 ? alert(`Shield wins.`) : alert(`Swords wins`);
+        }
     }
+}
+
+function isWin() {
+    let winStates = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 3]
+    ]
+
+    for(let i = 0; i < winStates.length; i++) {
+        let seq = winStates[i]
+    
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+
+        if (board[pos1] == board[pos2] &&
+            board[pos1] == board[pos3] &&
+            board[pos1] != '') {
+
+            return true;
+        }
+    }
+    return false;
 }
